57a21bbfbfe9f558bb9f143e18b2dd5c78dc0b0e
dabel <dabel@DESKTOP-JGCP78S>
2025-02-13 12:46:24 +0100

    modificacion en demo1 para generar conflicto con master

--- src/demo1/demo1.java
+++ src/demo1/demo1.java
@@ -2,6 +2,6 @@ package demo1;
 
 public class demo1 {
 	public void Main (String [] args) {
-		System.out.println("1");
+		System.out.println("wedbfeigbie"); //creamos conflicto pues modificamos la misma linea que en master
 	}
 }
