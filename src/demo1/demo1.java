85cb38738825240fc36c879898a61adf24dc86a3
dabel <dabel@DESKTOP-JGCP78S>
2025-02-13 12:50:05 +0100

    cambios realizados en master para generar conflictos con rama2

--- src/demo1/demo1.java
+++ src/demo1/demo1.java
@@ -2,6 +2,6 @@ package demo1;
 
 public class demo1 {
 	public void Main (String [] args) {
-		System.out.println("1");
+		System.out.println("12345"); //cambios realizados en master despues de rama2 conflictos
 	}
 }
